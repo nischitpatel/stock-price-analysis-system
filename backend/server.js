0f6e0b0c90fde14dc7a69635ab16140d2b410f6d
nischitpatel <np58845@gmail.com>
2025-12-01 10:53:09 -0500

    Updated cors

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(cors({
     origin: [
       "http://localhost:5173",
       "http://localhost:3000",
-      "https://stock-price-analysis-system.vercel.app/"
+      "https://stock-price-analysis-system.vercel.app"
     ],
     methods: "GET,POST",
     credentials: true
