e0d4c2face78d46df3aadc0a023aa495469ee29a
nischitpatel <np58845@gmail.com>
2025-12-21 00:33:40 -0500

    new backend integration

--- frontend/src/lib/apiClient.js
+++ frontend/src/lib/apiClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'site--tickerlense-backend--c2yp6pz4jpp6.code.run/api', timeout: 10000 });
+const api = axios.create({ baseURL: 'https://site--tickerlense-backend--c2yp6pz4jpp6.code.run/api', timeout: 10000 });
 // const api = axios.create({ baseURL: 'https://stock-price-analysis-system.onrender.com/api', timeout: 10000 });
 
 api.interceptors.response.use(
